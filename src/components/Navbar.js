5e6303fcc136604e0baa641e81d39c5ba19bc934
felipe neely <felodiud>
2023-06-03 11:45:34 -0400

    start

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar () {
+    return ( <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-body border-bottom box-shadow text-white">
+    <h5 className="my-0 mr-md-auto font-weight-normal"><strong>Start Bootstrap</strong></h5>
+    <nav className="my-2 my-md-0 mr-md-3 ">
+      <a className="p-2 text-white" href="#">Features</a>
+      <a className="p-2 text-white" href="#">Enterprise</a>
+      <a className="p-2 text-white" href="#">Support</a>
+      <a className="p-2 text-white" href="#">Pricing</a>
+    </nav>
+  </div>) 
+}
+
+export default Navbar; 
+
